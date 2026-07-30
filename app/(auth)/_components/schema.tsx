74305e167581e34ddbdbb6ace731fda4c1ea36ce
jessica <jessicadhamala20@gmail.com>
2026-07-30 11:19:25 +0545

    Update authentication validation schema

--- app/(auth)/_components/schema.tsx
+++ app/(auth)/_components/schema.tsx
@@ -2,7 +2,7 @@
 import { z } from "zod";
 
 export const registerSchema = z.object({
-    email: z.email("Invalid email address"),
+    email: z.string().email("Invalid email address"),
     firstName: z.string("Firstname must be string")
         .min(2, "First name must be at least 2 characters long"),
     lastName: z.string("Last name must be string")
@@ -21,7 +21,7 @@ export const registerSchema = z.object({
 export type RegisterFormData = z.infer<typeof registerSchema>;
 
 export const loginSchema = z.object({
-    email: z.email("Invalid email address"),
+    email: z.string().email("Invalid email address"),
     password: z.string("Password must be string")
         .min(6, "Password must be at least 6 characters long")
 });
